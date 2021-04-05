e4837566bb82c54928e1fcb4c50c8c0f609f3910
Spalash <palashtyagi167@gmail.com>
2021-04-05 01:32:23 +0100

    Delete slateTry.py

--- slateTry.py
+++
@@ -1,8 +0,0 @@
-import slate3k as slate
-readFileNamePDF = 'jarPhysReadFile.pdf'
-with open(readFileNamePDF,'rb') as f:
-    extracted_text = slate.PDF(f)
-    f.close()
-
-for x in range(0,len(extracted_text)):
-    print(x)
