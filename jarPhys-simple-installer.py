dd924c44f973e2531906d91d19c69b09be4d47fe
Spalash <palashtyagi167@gmail.com>
2021-04-25 03:10:44 +0100

    .

--- jarPhys-simple-installer.py
+++ jarPhys-simple-installer.py
@@ -2,9 +2,11 @@ import subprocess
 import sys
 import os
 import shutil
+import time
 
 subprocess.check_call(' '.join([sys.executable, "-m pip install requests"]))
 subprocess.check_call(' '.join([sys.executable, "-m pip install zipfile38"]))
+
 import requests 
 from zipfile38 import ZipFile
 
@@ -13,16 +15,23 @@ def download_zip(url, fileName='./main.zip', chunk_size=128):
     with open(fileName, 'wb') as fd:
         for chunk in r.iter_content(chunk_size=chunk_size):
             fd.write(chunk)
+    
     with ZipFile(fileName, 'r') as zipObj:
-        zipObj.extractall()    
+        zipObj.extractall()
+    
+    os.rename('./jarPhys-main','./jarPhys')  
     os.remove(fileName)
 
 download_zip('https://codeload.github.com/Magnus167/jarPhys/zip/refs/heads/main')
-
+print('...')
+time.sleep(3)
+time
 selfDir = os.getcwd()
-os.chdir('./jarPhys-main')
-subprocess.check_call(' '.join([sys.executable, " installer.py"]))
+os.chdir('./jarPhys')
+print('...')
+time.sleep(3)
+from jarPhys.installer import *
+installerX()
+shutil.rmtree('__pycache__')
 os.chdir(selfDir)
 os.remove('jarPhys-simple-installer.py')
-
-
